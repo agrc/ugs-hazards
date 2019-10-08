b4284531895137c2ce54a306efb923baafde2956
Scott Davis <stdavis@utah.gov>
2019-10-08 13:29:33 -0600

    fix: refactor constants into config and return all data for units

--- clientside/src/App.js
+++ clientside/src/App.js
@@ -1,32 +1,10 @@
 import React, { useState } from 'react';
 import './App.scss';
 import { stringify } from 'query-string';
+import stringifyObject from 'stringify-object';
+import config from './config';
 
 
-const baseUrl = 'https://services.arcgis.com/ZzrwjTRez6FJiOq4/arcgis/rest/services';
-const queries = [
-  ['Utah_Geologic_Hazards/FeatureServer/1', 'FLHHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/2', 'SGSHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/3', 'LSSHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/5', 'LSUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/6', 'CASHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/7', 'CSSHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/8', 'CRSHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/9', 'EFHHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/10', 'ERZHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/11', 'EXSHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/12', 'GSPHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/13', 'MKFHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/14', 'PESHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/15', 'GRSHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/16', 'RFHHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/17', 'SDHHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/18', 'SBPHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/19', 'SLSHazardUnit'],
-  ['Utah_Geologic_Hazards/FeatureServer/20', 'WSSHazardUnit'],
-  ['Utah_Earthquake_Hazards/FeatureServer/3', 'LQSHazardUnit'],
-  ['Utah_Earthquake_Hazards/FeatureServer/4', 'SFRHazardUnit'],
-];
 const defaultParameters = {
   geometryType: 'esriGeometryPolygon',
   returnGeometry: false,
@@ -39,45 +17,44 @@ export default props => {
   console.log('App');
 
   const [ units, setUnits ] = useState([]);
-  const makeRequest = async ([featureService, field]) => {
+  const makeRequest = async ([featureService, hazardCode]) => {
     const parameters = {
       geometry: JSON.stringify(props.aoi),
-      outFields: field,
+      outFields: "*",
       ...defaultParameters
     };
 
-    const response = await fetch(`${baseUrl}/${featureService}/query?${stringify(parameters)}`);
+    const response = await fetch(`${config.urls.baseUrl}/${featureService}/query?${stringify(parameters)}`);
     const responseJson = await response.json();
 
-    return (responseJson.features.length > 0) ? responseJson.features[0].attributes[field] : null;
-  };
-
-  const removeNulls = (previous, current) => {
-    if (current) {
-      previous.push(current);
-    }
-
-    return previous;
+    return [responseJson.features, hazardCode];
   };
 
   const getData = async (aoi) => {
-    const newUnits = await Promise.all(queries.map(makeRequest));
+    const newUnits = await Promise.all(config.queries.map(makeRequest));
 
-    setUnits(newUnits.reduce(removeNulls, []));
+    setUnits(newUnits);
   };
 
   if (units.length === 0) {
     getData(props.aoi);
   }
 
+  const stringifyParams = { indent: '  ' };
+
   return (
     <div className="app">
       <h3>Input polygon</h3>
-      <p className="code">{JSON.stringify(props.aoi)}</p>
+      <p className="code">{stringifyObject(props.aoi, stringifyParams)}</p>
       <h3>Hazards Found</h3>
-      <ul className="code">
-        { units.map((data, index) => <li key={index}>{data}</li>) }
-      </ul>
+      <div className="code">
+        { units.map(([features, hazardCode]) =>
+          <div key={hazardCode}>
+            <h4>{hazardCode}</h4>
+            <p>{stringifyObject(features, stringifyParams)}</p>
+          </div>)
+        }
+      </div>
     </div>
   );
 }
---
+++ clientside/src/config.js
@@ -0,0 +1,35 @@
+const baseUrl = 'https://services.arcgis.com/ZzrwjTRez6FJiOq4/arcgis/rest/services';
+export default {
+  urls: {
+    baseUrl,
+    hazardGroupingsTable: `${baseUrl}/Report_Tables_View/FeatureServer/0`
+  },
+  queries: [
+    // ['Utah_Geologic_Hazards/FeatureServer/0', '???'], // flood canyon
+    ['Utah_Geologic_Hazards/FeatureServer/1', 'FLH'],
+    ['Utah_Geologic_Hazards/FeatureServer/2', 'SGS'],
+    ['Utah_Geologic_Hazards/FeatureServer/3', 'LSS'],
+    // ['Utah_Geologic_Hazards/FeatureServer/4', '???'], // landslides
+    // ['Utah_Geologic_Hazards/FeatureServer/5', 'LSUnit'], // legacy landslide
+    ['Utah_Geologic_Hazards/FeatureServer/6', 'CAS'],
+    ['Utah_Geologic_Hazards/FeatureServer/7', 'CSS'],
+    ['Utah_Geologic_Hazards/FeatureServer/8', 'CRS'],
+    ['Utah_Geologic_Hazards/FeatureServer/9', 'EFH'],
+    ['Utah_Geologic_Hazards/FeatureServer/10', 'ERZ'],
+    ['Utah_Geologic_Hazards/FeatureServer/11', 'EXS'],
+    ['Utah_Geologic_Hazards/FeatureServer/12', 'GSP'],
+    ['Utah_Geologic_Hazards/FeatureServer/13', 'MKF'],
+    ['Utah_Geologic_Hazards/FeatureServer/14', 'PES'],
+    ['Utah_Geologic_Hazards/FeatureServer/15', 'GRS'],
+    ['Utah_Geologic_Hazards/FeatureServer/16', 'RFH'],
+    ['Utah_Geologic_Hazards/FeatureServer/17', 'SDH'],
+    ['Utah_Geologic_Hazards/FeatureServer/18', 'SBP'],
+    ['Utah_Geologic_Hazards/FeatureServer/19', 'SLS'],
+    ['Utah_Geologic_Hazards/FeatureServer/20', 'WSS'],
+    // ['Utah_Earthquake_Hazards/FeatureServer/0', '??'], // utah epicenters
+    // ['Utah_Earthquake_Hazards/FeatureServer/1', '??'], // mining-induced epicenters
+    // ['Utah_Earthquake_Hazards/FeatureServer/2', '??'], // quaternary faults
+    ['Utah_Earthquake_Hazards/FeatureServer/3', 'LQS'],
+    ['Utah_Earthquake_Hazards/FeatureServer/4', 'SFR'],
+  ]
+};
