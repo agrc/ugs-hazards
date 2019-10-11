07aba63089ff1f34ffebf93c319ffa4b6bf137fd
Scott Davis <stdavis@utah.gov>
2019-10-11 14:41:25 -0600

    feat: add html element titles to help determine where text comes from These should only show up in dev and staging.

--- src/reportParts/Group.js
+++ src/reportParts/Group.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import config from '../config';
 
 export default props => {
   return (
     <>
-      <h1>{props.name}</h1>
-      <p dangerouslySetInnerHTML={{ __html: props.text}}></p>
+      <h1 title={config.notProd && 'HazardGroupingsTable.HazardGroup'}>{props.name}</h1>
+      <p dangerouslySetInnerHTML={{ __html: props.text}} title={config.notProd && 'HazardGroupTextTable.Text'}></p>
       {props.children}
     </>
   );
--- src/reportParts/Hazard.js
+++ src/reportParts/Hazard.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { HazardMapContext } from './HazardMap';
+import config from '../config';
 
 
 export default props => {
@@ -10,8 +11,8 @@ export default props => {
 
   return (
     <>
-      <h2>{props.name}</h2>
-      <p dangerouslySetInnerHTML={{ __html: props.introText }} title='test title'></p>
+      <h2 title={config.notProd && 'HazardUnitTextTable.HazardName (from first unit)'}>{props.name}</h2>
+      <p dangerouslySetInnerHTML={{ __html: props.introText }} title={config.notProd && 'HazardIntroTextTable.Text'}></p>
       { visualAssets && <img src={visualAssets.mapImage}
         alt="map" style={{width: '100%', minHeight: '200px'}} /> }
       {props.children}
