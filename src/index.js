ff06d874222bbb14a247f0f7bce0371fe2e9c552
Scott Davis <stdavis@utah.gov>
2019-10-11 14:40:43 -0600

    feat: add environment variable for prod, staging and dev

--- src/index.js
+++ src/index.js
@@ -5,4 +5,7 @@ import App from './App';
 import polygonJson from './testPolygon.json';
 
 
+console.log(`app version: ${process.env.REACT_APP_VERSION || 'dev'}`);
+console.log(`environment: ${process.env.REACT_APP_ENVIRONMENT}`);
+
 ReactDOM.render(<App aoi={polygonJson} />, document.getElementById('root'));
