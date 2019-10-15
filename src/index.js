8b631f16840428d1f335cf0d05db8b3984aef5a9
Scott Davis <stdavis@utah.gov>
2019-10-15 11:02:15 -0600

    feat: add demo page to show how to pass data into the app
    Closes #13
    Co-authored-by: steveoh <sgourley@utah.gov>

--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import testDataJson from './testData.json';
 
 
 console.log(`app version: ${process.env.REACT_APP_VERSION || 'dev'}`);
 console.log(`environment: ${process.env.REACT_APP_ENVIRONMENT}`);
 
-ReactDOM.render(<App {...testDataJson} />, document.getElementById('root'));
+const init = (aoi) => {
+  console.log('aoi', aoi);
+
+  ReactDOM.render(<App {...aoi} />, document.getElementById('root'));
+};
+
+let aoi = localStorage.getItem('aoi');
+if (!aoi) {
+  import('./testData.json').then(aoi => init(aoi));
+} else {
+  init(JSON.parse(aoi));
+}
