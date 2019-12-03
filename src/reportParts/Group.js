3549ce86bcfe96b896d0cbcef93a27a6fb3a9dde
steveoh <sgourley@utah.gov>
2019-12-03 10:18:49 -0700

    fix: title and header page justification
    closes #22

--- src/reportParts/Group.js
+++ src/reportParts/Group.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import config from '../config';
+import './Group.scss';
 
 export default props => {
   return (
     <div className="page-break">
-      <h1 title={config.notProd && 'HazardGroupingsTable.HazardGroup'}>{props.name}</h1>
+      <h1 className="group__heading" title={config.notProd && 'HazardGroupingsTable.HazardGroup'}>{props.name}</h1>
       <p dangerouslySetInnerHTML={{ __html: props.text}} title={config.notProd && 'HazardGroupTextTable.Text'}></p>
       {props.children}
     </div>
--- src/reportParts/Hazard.js
+++ src/reportParts/Hazard.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { HazardMapContext } from './HazardMap';
 import config from '../config';
 import Loader from './Loader';
+import './Hazard.scss';
 
 
 export default props => {
@@ -12,7 +13,7 @@ export default props => {
 
   return (
     <div className="page-break">
-      <h2 title={config.notProd && 'HazardUnitTextTable.HazardName (from first unit)'}>{props.name}</h2>
+      <h2 className="hazard__heading" title={config.notProd && 'HazardUnitTextTable.HazardName (from first unit)'}>{props.name}</h2>
       <p dangerouslySetInnerHTML={{ __html: props.introText }} title={config.notProd && 'HazardIntroTextTable.Text'}></p>
       { visualAssets ? <img src={visualAssets.mapImage}
         alt="map" className="hazard-map-image" /> : <Loader /> }
