e1a97a8fb915d69826b41ae16acb0a9aa9cb4d5d
Scott Davis <stdavis@utah.gov>
2020-01-10 15:41:11 -0700

    feat: append "HAZARD" to each hazard group title Closes #53 #ALL_CAPS_FTW

--- src/reportParts/Group.js
+++ src/reportParts/Group.js
@@ -5,7 +5,7 @@ import './Group.scss';
 export default props => {
   return (
     <div className="page-break">
-      <h1 className="group__heading" title={config.notProd && 'HazardGroupingsTable.HazardGroup'}>{props.name}</h1>
+      <h1 className="group__heading" title={config.notProd && 'HazardGroupingsTable.HazardGroup'}>{props.name} Hazard</h1>
       <p dangerouslySetInnerHTML={{ __html: props.text}} title={config.notProd && 'HazardGroupTextTable.Text'}></p>
       {props.children}
     </div>
--- src/reportParts/Hazard.js
+++ src/reportParts/Hazard.js
@@ -9,7 +9,7 @@ import qffLegend from '../images/qff-legend.jpg';
 export default props => {
   return (
     <div className="page-break" id={kebabCase(props.name)}>
-      <h2 className="group__heading" title={config.notProd && "HazardGroupingsTable.HazardGroup (from parent)"}>{props.group}</h2>
+      <h2 className="group__heading" title={config.notProd && "HazardGroupingsTable.HazardGroup (from parent)"}>{props.group} Hazard</h2>
       <h2 className="hazard__heading" title={config.notProd && "HazardUnitTextTable.HazardName (from first unit)"}>{props.name}</h2>
       <p dangerouslySetInnerHTML={{ __html: props.introText }} title={config.notProd && "HazardIntroTextTable.Text"}></p>
       <MapSurround mapKey={props.code} />
